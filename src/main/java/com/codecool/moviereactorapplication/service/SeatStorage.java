598ae0c81758672609fa81f69b1e260910971bb8
dr. Simon Bálint <balladin0@gmail.com>
2020-06-08 18:50:23 +0200

    add basic seat storage functionality

--- src/main/java/com/codecool/moviereactorapplication/service/SeatStorage.java
+++ src/main/java/com/codecool/moviereactorapplication/service/SeatStorage.java
@@ -1,4 +1,39 @@
 package com.codecool.moviereactorapplication.service;
 
+import com.codecool.moviereactorapplication.model.Room;
+import com.codecool.moviereactorapplication.model.Seat;
+import lombok.Data;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class SeatStorage {
+    private SeatCreator seatCreator;
+    private @Getter List<Seat> seatStorage = new ArrayList<>();
+
+    @Autowired
+    public SeatStorage(SeatCreator seatCreator, Room room) {
+        this.seatCreator = seatCreator;
+        seatStorage = seatCreator.createSeatsForRoom(room, 2, 3);
+    }
+
+    public List<Seat> getSeatsByRoomId(int roomId) {
+        List<Seat> seats = new ArrayList<>();
+
+        for (Seat seat : seatStorage) {
+            if (seat.getRoom().getId() == roomId) {
+                seats.add(seat);
+            }
+        }
+        return seats;
+    }
+
+    public int getNumberOfSeatsByRoomId(int roomId) {
+        List<Seat> seats = getSeatsByRoomId(roomId);
+        return seats.size();
+    }
 }
