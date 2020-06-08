687133d628d8c20c6fa26d3a4dac45d2f57b9d89
dr. Simon Bálint <balladin0@gmail.com>
2020-06-08 16:58:39 +0200

    add lombok to room

--- src/main/java/com/codecool/moviereactorapplication/model/Room.java
+++ src/main/java/com/codecool/moviereactorapplication/model/Room.java
@@ -1,4 +1,15 @@
 package com.codecool.moviereactorapplication.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Room {
+    private Integer id;
+    private String name;
+    private Integer capacity;
+
 }
--- src/main/java/com/codecool/moviereactorapplication/model/Show.java
+++ src/main/java/com/codecool/moviereactorapplication/model/Show.java
@@ -3,12 +3,14 @@ package com.codecool.moviereactorapplication.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Service
 public class Show {
     private Integer id;
     private LocalDateTime showtime;
