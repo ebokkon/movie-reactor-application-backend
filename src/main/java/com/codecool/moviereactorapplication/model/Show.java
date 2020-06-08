92ff5ec79420f5d4b820248709bf9a9ed15387a7
dr. Simon Bálint <balladin0@gmail.com>
2020-06-08 16:33:35 +0200

    add init Show class with lombok

--- src/main/java/com/codecool/moviereactorapplication/model/Show.java
+++ src/main/java/com/codecool/moviereactorapplication/model/Show.java
@@ -1,4 +1,17 @@
 package com.codecool.moviereactorapplication.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Show {
+    private Integer id;
+    private LocalDateTime showtime;
+    private Room room;
+
 }
