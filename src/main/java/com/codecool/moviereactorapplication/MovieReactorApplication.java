11040108fe84b373147f152dd151da9f71c03634
Kornél <leitner.kornel@gmail.com>
2020-06-08 15:58:12 +0200

    Initial commit

---
+++ src/main/java/com/codecool/moviereactorapplication/MovieReactorApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.moviereactorapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReactorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieReactorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/moviereactorapplication/MovieReactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.moviereactorapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReactorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
