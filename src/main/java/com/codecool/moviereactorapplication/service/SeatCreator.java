fc3cb05e4b98c6d04ba3fc934b860d03abc4ac70
dr. Simon Bálint <balladin0@gmail.com>
2020-06-08 18:49:57 +0200

    add way to automatically create all seats for an auditorium room

--- src/main/java/com/codecool/moviereactorapplication/service/SeatCreator.java
+++ src/main/java/com/codecool/moviereactorapplication/service/SeatCreator.java
@@ -1,4 +1,26 @@
 package com.codecool.moviereactorapplication.service;
 
+import com.codecool.moviereactorapplication.model.Room;
+import com.codecool.moviereactorapplication.model.Seat;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class SeatCreator {
+
+    public List<Seat> createSeatsForRoom(Room room, int numberOfRows, int numberOfSeatsPerRow) {
+        List<Seat> seats = new ArrayList<>();
+        int seatId = 0;
+        for (int row = 1; row < numberOfRows; row++) {
+            for (int seatNu = 1; seatNu < numberOfSeatsPerRow; seatNu++) {
+                Seat newSeat = new Seat(seatId, row, seatNu, room);
+                seats.add(newSeat);
+                seatId++;
+            }
+        }
+        return seats;
+    }
+
 }
