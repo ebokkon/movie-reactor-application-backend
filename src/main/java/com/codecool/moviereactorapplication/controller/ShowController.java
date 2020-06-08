a88a936ba2660d3f65659465547064053a1e5132
Kornél <leitner.kornel@gmail.com>
2020-06-08 23:05:01 +0200

    Created getShowById method on route /show/showId - return Empty if error

--- src/main/java/com/codecool/moviereactorapplication/controller/ShowController.java
+++ src/main/java/com/codecool/moviereactorapplication/controller/ShowController.java
@@ -4,6 +4,7 @@ import com.codecool.moviereactorapplication.model.Show;
 import com.codecool.moviereactorapplication.service.ShowStorage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,6 +16,20 @@ public class ShowController {
     private ShowStorage showStorage;
 
     @GetMapping("/schedule")
-    public List<Show> allShows() {return showStorage.getShows();}
+    public List<Show> allShows() {
+        return showStorage.getShows();
+    }
+
+    @GetMapping("/show/{showId}")
+    public Show getShowById(@PathVariable String showId) {
+        try {
+            Integer id = Integer.parseInt(showId);
+            return showStorage.getShowById(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new Show();
+    }
+
 
 }
