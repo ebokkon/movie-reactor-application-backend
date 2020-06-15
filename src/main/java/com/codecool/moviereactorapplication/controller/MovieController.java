f671b98e64495062735244de66d639af995687ea
Kornél <leitner.kornel@gmail.com>
2020-06-15 16:00:11 +0200

    Delete @Autowired and move @CrossOrigin onto @RestController

--- src/main/java/com/codecool/moviereactorapplication/controller/MovieController.java
+++ src/main/java/com/codecool/moviereactorapplication/controller/MovieController.java
@@ -2,24 +2,22 @@ package com.codecool.moviereactorapplication.controller;
 
 import com.codecool.moviereactorapplication.model.Movie;
 import com.codecool.moviereactorapplication.service.MovieStorage;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class MovieController {
 
     private final MovieStorage movieStorage;
 
-    @Autowired
     public MovieController(MovieStorage movieStorage) {
         this.movieStorage = movieStorage;
     }
 
-    @CrossOrigin
     @GetMapping("/scheduled-movies")
     public List<Movie> getAllScheduledMovie() {
         return movieStorage.getAllMovie();
