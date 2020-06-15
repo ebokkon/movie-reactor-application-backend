23519a54506212f5912c5df452c31a8db01194c0
Kornél <leitner.kornel@gmail.com>
2020-06-15 16:18:36 +0200

    Remove @Autowired, make fields final, remove static from index

--- src/main/java/com/codecool/moviereactorapplication/service/SeatReservedCreator.java
+++ src/main/java/com/codecool/moviereactorapplication/service/SeatReservedCreator.java
@@ -3,7 +3,6 @@ package com.codecool.moviereactorapplication.service;
 import com.codecool.moviereactorapplication.model.Seat;
 import com.codecool.moviereactorapplication.model.SeatReserved;
 import com.codecool.moviereactorapplication.model.Show;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -12,15 +11,12 @@ import java.util.Random;
 
 @Component
 public class SeatReservedCreator {
-
-    //    private RoomStorage roomStorage;
-    private SeatStorage seatStorage;
-    private ShowStorage showStorage;
+    private final SeatStorage seatStorage;
+    private final ShowStorage showStorage;
     List<SeatReserved> reservedSeats = new ArrayList<>();
     private Random random = new Random();
-    private static int index = 0;
+    private int index = 0;
 
-    @Autowired
     public SeatReservedCreator(SeatStorage seatStorage, ShowStorage showStorage) {
         this.seatStorage = seatStorage;
         this.showStorage = showStorage;
