4ad69dbc8a753477ca18365db9ad6af71b742b6e
Kornél <leitner.kornel@gmail.com>
2020-06-15 16:11:48 +0200

    Remove @Component

--- src/main/java/com/codecool/moviereactorapplication/model/Seat.java
+++ src/main/java/com/codecool/moviereactorapplication/model/Seat.java
@@ -3,12 +3,10 @@ package com.codecool.moviereactorapplication.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Component
 public class Seat {
 
     private int id;
