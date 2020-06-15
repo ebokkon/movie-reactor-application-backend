4373ba65d2139d55ea2243e2940cd7af58fb7b48
Kornél <leitner.kornel@gmail.com>
2020-06-15 16:13:41 +0200

    Remove @Service

--- src/main/java/com/codecool/moviereactorapplication/model/Show.java
+++ src/main/java/com/codecool/moviereactorapplication/model/Show.java
@@ -3,7 +3,6 @@ package com.codecool.moviereactorapplication.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -11,7 +10,6 @@ import java.time.LocalTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Service
 public class Show {
     private Integer id;
     private LocalDate startingDate;
