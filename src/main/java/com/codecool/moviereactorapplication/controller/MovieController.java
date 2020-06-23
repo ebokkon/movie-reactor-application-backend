5c555e81025246a74581f5113469440d2197c2a2
Kornél <leitner.kornel@gmail.com>
2020-06-23 19:21:00 +0200

    Remove former Storage solution

--- src/main/java/com/codecool/moviereactorapplication/controller/MovieController.java
+++ src/main/java/com/codecool/moviereactorapplication/controller/MovieController.java
@@ -2,7 +2,6 @@ package com.codecool.moviereactorapplication.controller;
 
 import com.codecool.moviereactorapplication.entity.Movie;
 import com.codecool.moviereactorapplication.repository.MovieRepository;
-import com.codecool.moviereactorapplication.service.MovieStorage;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,17 +12,14 @@ import java.util.List;
 @RestController
 public class MovieController {
 
-    private final MovieStorage movieStorage;
     private final MovieRepository movieRepository;
 
-    public MovieController(MovieStorage movieStorage, MovieRepository movieRepository) {
-        this.movieStorage = movieStorage;
+    public MovieController(MovieRepository movieRepository) {
         this.movieRepository = movieRepository;
     }
 
     @GetMapping("/scheduled-movies")
     public List<Movie> getAllScheduledMovie() {
         return movieRepository.findAll();
-        //return movieStorage.getAllMovie();
     }
 }
