7edd51d0c1b8acd0fb37a63bcd776b9ef4b3ad55
Kornél <leitner.kornel@gmail.com>
2020-06-23 19:57:03 +0200

    Delete unused method

--- src/main/java/com/codecool/moviereactorapplication/service/RoomCreator.java
+++ src/main/java/com/codecool/moviereactorapplication/service/RoomCreator.java
@@ -2,7 +2,6 @@ package com.codecool.moviereactorapplication.service;
 
 import com.codecool.moviereactorapplication.entity.Room;
 import com.codecool.moviereactorapplication.repository.RoomRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -23,7 +22,6 @@ public class RoomCreator {
     }
 
     public void createNumberedRooms(int numberOfRooms) {
-        System.out.println("This is the room creator.");
         List<Room> rooms = new ArrayList<>();
         for (int roomId = 1; roomId < numberOfRooms+1; roomId++) {
             String name = roomNames.get(random.nextInt(roomNames.size()));
@@ -34,21 +32,7 @@ public class RoomCreator {
                     .build();
             newRoom.setCapacity();
             rooms.add(newRoom);
-            System.out.println(newRoom.toString());
         }
         roomRepository.saveAll(rooms);
-    }
-
-
-    public List<com.codecool.moviereactorapplication.model.Room> createRooms(int numberOfRooms) {
-        System.out.println("This is the room creator.");
-        List<com.codecool.moviereactorapplication.model.Room> rooms = new ArrayList<>();
-        for (int roomId = 1; roomId < numberOfRooms + 1; roomId++) {
-            String name = roomNames.get(random.nextInt(roomNames.size()));
-            com.codecool.moviereactorapplication.model.Room newRoom = new com.codecool.moviereactorapplication.model.Room(roomId, name, 5, 9);
-            rooms.add(newRoom);
-            System.out.println(newRoom.toString());
-        }
-        return rooms;
     }
 }
