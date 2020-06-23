3f6441c3c2abcaa21b1716f2b9be699c86f4c047
Kornél <leitner.kornel@gmail.com>
2020-06-23 16:04:44 +0200

    Get data from database instead of Storage

--- src/main/java/com/codecool/moviereactorapplication/controller/MovieController.java
+++ src/main/java/com/codecool/moviereactorapplication/controller/MovieController.java
@@ -1,6 +1,7 @@
 package com.codecool.moviereactorapplication.controller;
 
-import com.codecool.moviereactorapplication.model.Movie;
+import com.codecool.moviereactorapplication.entity.Movie;
+import com.codecool.moviereactorapplication.repository.MovieRepository;
 import com.codecool.moviereactorapplication.service.MovieStorage;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,13 +14,16 @@ import java.util.List;
 public class MovieController {
 
     private final MovieStorage movieStorage;
+    private final MovieRepository movieRepository;
 
-    public MovieController(MovieStorage movieStorage) {
+    public MovieController(MovieStorage movieStorage, MovieRepository movieRepository) {
         this.movieStorage = movieStorage;
+        this.movieRepository = movieRepository;
     }
 
     @GetMapping("/scheduled-movies")
     public List<Movie> getAllScheduledMovie() {
-        return movieStorage.getAllMovie();
+        return movieRepository.findAll();
+        //return movieStorage.getAllMovie();
     }
 }
