0960ea64ac2e7b4e07e728ce9510d5d89a262d9d
Kornél <leitner.kornel@gmail.com>
2020-06-23 19:27:39 +0200

    Set entity relation

--- src/main/java/com/codecool/moviereactorapplication/entity/Room.java
+++ src/main/java/com/codecool/moviereactorapplication/entity/Room.java
@@ -1,11 +1,9 @@
 package com.codecool.moviereactorapplication.entity;
 
-import com.codecool.moviereactorapplication.entity.Seat;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.*;
 
 import javax.persistence.*;
-import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -19,8 +17,6 @@ public class Room {
     private Long id;
 
     private String name;
-
-    // TODO: Should create a transient field capacity, calculated by the number of rows and seats per row.
 
     private Integer numberOfRows;
     private Integer numberOfSeatsPerRow;
@@ -38,7 +34,9 @@ public class Room {
     @OneToMany(mappedBy = "room", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
     private List<Seat> seats;
 
-    /*public Room() {
-        this.capacity = numberOfRows * numberOfSeatsPerRow;
-    }*/
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @JsonManagedReference
+    @OneToMany(mappedBy = "room", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private List<Show> shows;
 }
