e73371d1dbe60d91ce4b8d439c605969d9657f61
Kornél <leitner.kornel@gmail.com>
2020-06-23 19:51:22 +0200

    Change show creator method, store shows in database

--- src/main/java/com/codecool/moviereactorapplication/service/ShowCreator.java
+++ src/main/java/com/codecool/moviereactorapplication/service/ShowCreator.java
@@ -1,49 +1,45 @@
 package com.codecool.moviereactorapplication.service;
 
-import com.codecool.moviereactorapplication.model.Movie;
-import com.codecool.moviereactorapplication.model.Room;
-import com.codecool.moviereactorapplication.model.Show;
+import com.codecool.moviereactorapplication.entity.Movie;
+import com.codecool.moviereactorapplication.entity.Show;
+import com.codecool.moviereactorapplication.repository.MovieRepository;
+import com.codecool.moviereactorapplication.repository.RoomRepository;
+import com.codecool.moviereactorapplication.repository.SeatReservedRepository;
+import com.codecool.moviereactorapplication.repository.ShowRepository;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Component
 public class ShowCreator {
-    private final List<Movie> movieStorage;
-    private final RoomStorage roomStorage;
-    private int showId = 1;
+    private final ShowRepository showRepository;
+    private final RoomRepository roomRepository;
+    private final MovieRepository movieRepository;
+    private final SeatReservedRepository seatReservedRepository;
 
-    public ShowCreator(MovieStorage movieStorage, RoomStorage roomStorage) {
-        this.movieStorage = movieStorage.getMovieStorage();
-        this.roomStorage = roomStorage;
+    public ShowCreator(ShowRepository showRepository, RoomRepository roomRepository, MovieRepository movieRepository, SeatReservedRepository seatReservedRepository) {
+        this.showRepository = showRepository;
+        this.roomRepository = roomRepository;
+        this.movieRepository = movieRepository;
+        this.seatReservedRepository = seatReservedRepository;
     }
 
-    public List<Show> createShows() {
-        List<Show> createdShows = new ArrayList<>();
-        LocalDate startingDate = LocalDate.of(2020, 6, 20);
-        LocalTime startingTime = LocalTime.of(14, 30);
-
-        for (Movie movie : movieStorage) {
-            createdShows.add(new Show(showId++, startingDate, startingTime, movie, new Room()));
-        }
-        return createdShows;
-    }
-
-    public List<Show> createWeeklySchedule(LocalDate fromDate) {
-        List<Show> createdSchedule = new ArrayList<>();
-
+    public void createWeeklyScheduleData(LocalDate fromDate) {
         for (int i = 0; i < 7; i++) {
             LocalTime startingTime = LocalTime.of(12, 0);
-            for (Movie movie : movieStorage) {
-                createdSchedule.add(new Show(showId++, fromDate, startingTime, movie, roomStorage.getRoomById(1)));
+            for (Movie movie : movieRepository.findAll()) {
+                Show currentShow = Show.builder()
+                        .movie(movie)
+                        .startingDate(fromDate)
+                        .startingTime(startingTime)
+                        .room(roomRepository.findAll().get(0))
+                        .build();
+                currentShow.setReservedSeats(seatReservedRepository.getReservedSeatsByShowId(currentShow.getId()));
+                showRepository.save(currentShow);
                 startingTime = startingTime.plusHours(2);
             }
             fromDate = fromDate.plusDays(1);
         }
-
-        return createdSchedule;
     }
 }
