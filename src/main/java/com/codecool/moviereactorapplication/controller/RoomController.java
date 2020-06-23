58432189763950c43c2fe57dee21200ffcca8b39
Kornél <leitner.kornel@gmail.com>
2020-06-23 16:05:04 +0200

    Get data from database instead of Storage

--- src/main/java/com/codecool/moviereactorapplication/controller/RoomController.java
+++ src/main/java/com/codecool/moviereactorapplication/controller/RoomController.java
@@ -1,8 +1,11 @@
 package com.codecool.moviereactorapplication.controller;
 
-import com.codecool.moviereactorapplication.model.Room;
+import com.codecool.moviereactorapplication.entity.Room;
+import com.codecool.moviereactorapplication.repository.RoomRepository;
 import com.codecool.moviereactorapplication.service.RoomStorage;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @CrossOrigin
 @RestController
@@ -10,13 +13,16 @@ import org.springframework.web.bind.annotation.*;
 public class RoomController {
 
     private final RoomStorage roomStorage;
+    private final RoomRepository roomRepository;
 
-    public RoomController(RoomStorage roomStorage) {
+    public RoomController(RoomStorage roomStorage, RoomRepository roomRepository) {
         this.roomStorage = roomStorage;
+        this.roomRepository = roomRepository;
     }
 
     @GetMapping("/{id}")
-    public Room getRoom(@PathVariable("id") Integer id) {
-        return roomStorage.getRoomById(id);
+    public Optional<Room> getRoom(@PathVariable("id") Integer id) {
+        return roomRepository.findById(Long.valueOf(id));
+        //return roomStorage.getRoomById(id);
     }
 }
