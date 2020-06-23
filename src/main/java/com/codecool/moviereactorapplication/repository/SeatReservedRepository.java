4c035619f4f451d3630ba69106b423fc785e58b1
Kornél <leitner.kornel@gmail.com>
2020-06-23 19:39:01 +0200

    Create new query method

--- src/main/java/com/codecool/moviereactorapplication/repository/SeatReservedRepository.java
+++ src/main/java/com/codecool/moviereactorapplication/repository/SeatReservedRepository.java
@@ -2,6 +2,8 @@ package com.codecool.moviereactorapplication.repository;
 
 import com.codecool.moviereactorapplication.entity.SeatReserved;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface SeatReservedRepository extends JpaRepository<SeatReserved, Long> {
+    List<SeatReserved> getReservedSeatsByShowId(Long showId);
 }
