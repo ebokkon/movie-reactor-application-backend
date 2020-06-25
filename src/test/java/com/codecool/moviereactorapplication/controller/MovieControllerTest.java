d4a3c7c1f32eb73775008b469725a047e16bd707
Kornél <leitner.kornel@gmail.com>
2020-06-25 12:48:24 +0200

    Create testZeroMovieIsScheduled() test case for MovieController

--- src/test/java/com/codecool/moviereactorapplication/controller/MovieControllerTest.java
+++ src/test/java/com/codecool/moviereactorapplication/controller/MovieControllerTest.java
@@ -32,4 +32,9 @@ class MovieControllerTest {
 
         assertEquals(movieRepository.findAll(), movieController.getAllScheduledMovie());
     }
+
+    @Test
+    void testZeroMovieIsScheduled() {
+        assertEquals(movieRepository.findAll(), movieController.getAllScheduledMovie());
+    }
 }
