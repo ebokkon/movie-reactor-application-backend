782d13f79859687464f3fd190e7faf42f68b59f5
Eszter Bokkon <bokkone@gmail.com>
2020-07-27 15:25:33 +0200

    Create CustomUserDetailsService

---
+++ src/main/java/com/codecool/moviereactorapplication/security/CustomUserDetailsService.java
@@ -0,0 +1,46 @@
+package com.codecool.moviereactorapplication.security;
+
+import com.codecool.moviereactorapplication.entity.Visitor;
+import com.codecool.moviereactorapplication.repository.VisitorRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private VisitorRepository visitors;
+
+    public CustomUserDetailsService(VisitorRepository visitors) {
+        this.visitors = visitors;
+    }
+
+    /**
+     * Loads the user from the DB and converts it to Spring Security's internal User object.
+     * Spring will call this code to retrieve a user upon login from the DB.
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Visitor visitor = visitors.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username: " + username + " not found"));
+        return new User(visitor.getUsername(), visitor.getPassword(),
+                visitor.getRoles().stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
+    }
+
+    public String getCurrentUsername() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username;
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails) principal).getUsername();
+        } else {
+            username = principal.toString();
+        }
+        return username;
+    }
+}
