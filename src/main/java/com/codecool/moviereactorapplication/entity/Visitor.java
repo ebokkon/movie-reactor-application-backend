82bee313773aada56c1a1bc252fa81952b9a009c
Eszter Bokkon <bokkone@gmail.com>
2020-07-27 08:25:58 +0200

    Create Visitor entity.

---
+++ src/main/java/com/codecool/moviereactorapplication/entity/Visitor.java
@@ -0,0 +1,36 @@
+package com.codecool.moviereactorapplication.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Visitor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String firstname;
+
+    private String lastname;
+
+    private String email;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
+}
