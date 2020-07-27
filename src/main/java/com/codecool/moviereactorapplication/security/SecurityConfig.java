07570a2a41816b4c6ff04bd7a6764c574ac21806
Eszter Bokkon <bokkone@gmail.com>
2020-07-27 15:24:36 +0200

    Implement AuthenticationManagerBean in SecurityConfig

--- src/main/java/com/codecool/moviereactorapplication/security/SecurityConfig.java
+++ src/main/java/com/codecool/moviereactorapplication/security/SecurityConfig.java
@@ -1,13 +1,22 @@
-package com.codecool.moviereactorapplication.config;
+package com.codecool.moviereactorapplication.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenServices jwtTokenServices;
+
+    public SecurityConfig(JwtTokenServices jwtTokenServices) {
+        this.jwtTokenServices = jwtTokenServices;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -18,7 +27,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers("/auth/signin").permitAll() // allowed by anyone
+                .antMatchers("/me").authenticated()
                 .antMatchers(HttpMethod.GET, "/scheduled-movies").authenticated() // allowed only when signed in
-                .anyRequest().denyAll(); // anything else is denied
+                .anyRequest().denyAll() // anything else is denied
+                .and()
+                .addFilterBefore(new JwtTokenFilter(jwtTokenServices), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
