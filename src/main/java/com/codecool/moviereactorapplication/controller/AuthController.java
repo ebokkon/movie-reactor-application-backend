a6b92ef210503432d39c56fdeac7c72660be279a
Eszter Bokkon <bokkone@gmail.com>
2020-07-28 16:51:13 +0200

    Implplement register(sign up) method in AuthController

--- src/main/java/com/codecool/moviereactorapplication/controller/AuthController.java
+++ src/main/java/com/codecool/moviereactorapplication/controller/AuthController.java
@@ -1,7 +1,10 @@
 package com.codecool.moviereactorapplication.controller;
 
+import com.codecool.moviereactorapplication.entity.Visitor;
+import com.codecool.moviereactorapplication.model.Gender;
 import com.codecool.moviereactorapplication.model.UserCredentials;
 import com.codecool.moviereactorapplication.repository.VisitorRepository;
+import com.codecool.moviereactorapplication.security.DataValidatorService;
 import com.codecool.moviereactorapplication.security.JwtTokenServices;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,27 +13,36 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
+    private final VisitorRepository allVisitors;
+
     private final AuthenticationManager authenticationManager;
 
     private final JwtTokenServices jwtTokenServices;
 
-    public AuthController(AuthenticationManager authenticationManager, JwtTokenServices jwtTokenServices, VisitorRepository visitors) {
+    private final DataValidatorService dataValidator;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthController(AuthenticationManager authenticationManager, JwtTokenServices jwtTokenServices, VisitorRepository visitors, VisitorRepository allVisitors, DataValidatorService dataValidator) {
         this.authenticationManager = authenticationManager;
         this.jwtTokenServices = jwtTokenServices;
+        this.allVisitors = allVisitors;
+        this.dataValidator = dataValidator;
+        passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     @PostMapping("/login")
@@ -57,8 +69,84 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    public void register(@RequestBody UserCredentials data) {
-        // TODO: Need to implement, return ResponseEntity
+    public ResponseEntity register(@RequestBody UserCredentials userCredentials) {
+        String username = userCredentials.getUsername();
+        String password = userCredentials.getPassword();
+        String firstname = userCredentials.getFirstName();
+        String lastname = userCredentials.getLastName();
+        String email = userCredentials.getEmail();
+        Gender gender = userCredentials.getGender();
+
+        Map<Object, Object> model = new HashMap<>();
+        List<String> errorList = new ArrayList<>();
+
+        if (allVisitors.findByUsername(username).isPresent()){
+            model.put("correct", false);
+            model.put("msg", "Username already exists! Please choose a different username!");
+            return ResponseEntity.ok(model);
+        }
+        if(!dataValidator.isValidUsername(username, errorList)) {
+            String error = String.join(" , ", errorList);
+            String errorMessage = "Username should have " + error + "!";
+            model.put("correct", false);
+            model.put("msg", errorMessage);
+            return ResponseEntity.ok(model);
+        }
+
+        if (!dataValidator.isValidPassword(password, errorList)) {
+            String error = String.join(" , ", errorList);
+            String errorMessage = "Password should contain " + error + "!";
+            model.put("correct", false);
+            model.put("msg", errorMessage);
+            return ResponseEntity.ok(model);
+        }
+
+        if (dataValidator.isValidName(firstname, errorList)) {
+            String error = String.join(" , ", errorList);
+            String errorMessage = "Firstname " + error + "!";
+            model.put("correct", false);
+            model.put("msg", errorMessage);
+            return ResponseEntity.ok(model);
+        }
+
+        if (dataValidator.isValidName(lastname, errorList)) {
+            String error = String.join(" , ", errorList);
+            String errorMessage = "Lastname " + error + "!";
+            model.put("correct", false);
+            model.put("msg", errorMessage);
+            return ResponseEntity.ok(model);
+        }
+
+        if (!dataValidator.isValidEmail(email, errorList)) {
+            model.put("correct", false);
+            model.put("msg", "Not valid email!");
+            return ResponseEntity.ok(model);
+        }
+
+        if (!dataValidator.isValidGender(gender, errorList)) {
+            model.put("correct", false);
+            model.put("msg", "Not related data!");
+            return ResponseEntity.ok(model);
+        }
+
+        Visitor newVisitor = Visitor.builder()
+                .username(username)
+                .password(passwordEncoder.encode(password))
+                .firstname(firstname)
+                .lastname(lastname)
+                .email(email)
+                .gender(gender)
+                .roles(Collections.singletonList("ROLE_USER"))
+                .build();
+        allVisitors.save(newVisitor);
+
+        List<String> roles = Collections.singletonList("ROLE_USER");
+        String token = jwtTokenServices.createToken(username, roles);
+        model.put("correct", true);
+        model.put("username", username);
+        model.put("roles", roles);
+        model.put("token", token);
+        return ResponseEntity.ok(model);
     }
 }
 
