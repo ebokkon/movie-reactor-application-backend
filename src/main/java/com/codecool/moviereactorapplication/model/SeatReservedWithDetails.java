6dd3915ed87aaafd26be4f5b0678a6e2f168c4f1
Kornél <leitner.kornel@gmail.com>
2020-07-28 22:58:48 +0200

    Create process Class for Constructor Expressions

---
+++ src/main/java/com/codecool/moviereactorapplication/model/SeatReservedWithDetails.java
@@ -0,0 +1,25 @@
+package com.codecool.moviereactorapplication.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Component
+public class SeatReservedWithDetails {
+
+    private Long id;
+    private Long showId;
+    private LocalDate startingDate;
+    private LocalTime startingTime;
+    private Integer movieDbId;
+
+}
