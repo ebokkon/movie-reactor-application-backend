f788565698be0a2f9a12260db33fb58e09449a66
Eszter Bokkon <bokkone@gmail.com>
2020-07-29 12:14:47 +0200

    Implement findAll() method in VisitorRepository

--- src/main/java/com/codecool/moviereactorapplication/repository/VisitorRepository.java
+++ src/main/java/com/codecool/moviereactorapplication/repository/VisitorRepository.java
@@ -4,10 +4,13 @@ import com.codecool.moviereactorapplication.entity.Visitor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface VisitorRepository extends JpaRepository<Visitor, Long> {
 
     Optional<Visitor> findByUsername(String username);
+
+    List<Visitor> findAll();
 }
