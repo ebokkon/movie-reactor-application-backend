2e6a875a30a6f13a450e5b0d9951b95720d35145
Eszter Bokkon <bokkone@gmail.com>
2020-07-29 12:12:03 +0200

    Add @CrossOrigin annotation to AuthController

--- src/main/java/com/codecool/moviereactorapplication/controller/AuthController.java
+++ src/main/java/com/codecool/moviereactorapplication/controller/AuthController.java
@@ -8,21 +8,18 @@ import com.codecool.moviereactorapplication.security.DataValidatorService;
 import com.codecool.moviereactorapplication.security.JwtTokenServices;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
+@CrossOrigin(origins="${main.route}")
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
