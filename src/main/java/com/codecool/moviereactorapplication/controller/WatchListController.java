f6736bcff70c0214b9c8bf034b89e5e555833b10
Eszter Bokkon <bokkone@gmail.com>
2020-07-30 09:56:11 +0200

    Modify WatchListController

--- src/main/java/com/codecool/moviereactorapplication/controller/WatchListController.java
+++ src/main/java/com/codecool/moviereactorapplication/controller/WatchListController.java
@@ -1,20 +1,39 @@
 package com.codecool.moviereactorapplication.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.codecool.moviereactorapplication.entity.Visitor;
+import com.codecool.moviereactorapplication.repository.VisitorRepository;
+import com.codecool.moviereactorapplication.security.CustomUserDetailsService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController("/watchlist")
 public class WatchListController {
 
-    @GetMapping("/user")
-    public void getWatchListByUser() {
-        // TODO: Need to implement and its Repositories, entities. PathVariable
+    private final VisitorRepository allVisitors;
+
+    private final CustomUserDetailsService customUserDetailsService;
+
+    public WatchListController(VisitorRepository allVisitors, CustomUserDetailsService customUserDetailsService) {
+        this.allVisitors = allVisitors;
+        this.customUserDetailsService = customUserDetailsService;
     }
 
-    @GetMapping("/save")
-    public void saveMovieIntoWatchList() {
-        // TODO: Need to implement and its Repositories, entities. RequestBody
-    }
+//    @GetMapping("/user/{id}")
+//    public List<Long> getWatchListByUser(@PathVariable("user_id") Long userId) {
+//        return allVisitors.getWatchListByUser(userId);
+//    }
+//
+//    @GetMapping("/save")
+//    public void saveMovieIntoWatchList(@RequestParam("movie_id") Long movieId) {
+//        String username = customUserDetailsService.findLoggedInUsername();
+//        if (username != null) {
+//            Visitor user = allVisitors.findUserByUsername(username);
+//            List<Long> watchList = allVisitors.getWatchListByUser(user.getId());
+//            watchList.add(movieId);
+//            user.setWatchListMovieIds(watchList);
+//            allVisitors.save(user);
+//        }
+//    }
 }
