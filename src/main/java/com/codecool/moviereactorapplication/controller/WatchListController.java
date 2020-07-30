d16f7e43443350caa19fce22676dc0afecf7a466
Kornel Leitner <leitner.kornel@gmail.com>
2020-07-30 11:42:39 +0200

    Merged second solution for save into watchlist

--- src/main/java/com/codecool/moviereactorapplication/controller/WatchListController.java
+++ src/main/java/com/codecool/moviereactorapplication/controller/WatchListController.java
@@ -8,58 +8,33 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import com.codecool.moviereactorapplication.entity.Visitor;
-import com.codecool.moviereactorapplication.repository.VisitorRepository;
-import com.codecool.moviereactorapplication.security.CustomUserDetailsService;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import com.codecool.moviereactorapplication.security.CustomUserDetailsService;
 
 @CrossOrigin
 @RestController("/watchlist")
 public class WatchListController {
 
     private final VisitorRepository visitorRepository;
+    private final CustomUserDetailsService customUserDetailsService;
 
-    public WatchListController(VisitorRepository visitorRepository){
+    public WatchListController(VisitorRepository visitorRepository, CustomUserDetailsService customUserDetailsService){
         this.visitorRepository=visitorRepository;
+        this.customUserDetailsService = customUserDetailsService;
     }
 
     @GetMapping("/user")
     public List<Map<String,Integer>> getWatchListByUser(@RequestHeader String username) {
-        List<Map<String,Integer>> returnList=new ArrayList<>();
-        List<Integer> idList=visitorRepository.getByUsername(username).getWatchList();
-        for(int id : idList){
-            Map<String,Integer> data=new HashMap<>();
-            data.put("id",id);
+        List<Map<String, Integer>> returnList = new ArrayList<>();
+        List<Integer> idList = visitorRepository.getByUsername(username).getWatchList();
+        for (int id : idList) {
+            Map<String, Integer> data = new HashMap<>();
+            data.put("id", id);
             returnList.add(data);
         }
         return returnList;
-    private final VisitorRepository allVisitors;
-
-    private final CustomUserDetailsService customUserDetailsService;
-
-    public WatchListController(VisitorRepository allVisitors, CustomUserDetailsService customUserDetailsService) {
-        this.allVisitors = allVisitors;
-        this.customUserDetailsService = customUserDetailsService;
     }
 
-//    @GetMapping("/user/{id}")
-//    public List<Long> getWatchListByUser(@PathVariable("user_id") Long userId) {
-//        return allVisitors.getWatchListByUser(userId);
-//    }
-//
-//    @GetMapping("/save")
-//    public void saveMovieIntoWatchList(@RequestParam("movie_id") Long movieId) {
-//        String username = customUserDetailsService.findLoggedInUsername();
-//        if (username != null) {
-//            Visitor user = allVisitors.findUserByUsername(username);
-//            List<Long> watchList = allVisitors.getWatchListByUser(user.getId());
-//            watchList.add(movieId);
-//            user.setWatchListMovieIds(watchList);
-//            allVisitors.save(user);
-//        }
-//    }
     @PostMapping("/save/{movie_db_id}")
     public boolean saveMovieIntoWatchList(@RequestHeader String username,@PathVariable Integer movie_db_id) {
         Visitor visitor=visitorRepository.getByUsername(username);
@@ -85,4 +60,23 @@ public class WatchListController {
         visitorRepository.save(visitor);
         return true;
     }
+
+
+//    @GetMapping("/user/{id}")
+//    public List<Long> getWatchListByUser(@PathVariable("user_id") Long userId) {
+//        return allVisitors.getWatchListByUser(userId);
+//    }
+//
+//    @GetMapping("/save")
+//    public void saveMovieIntoWatchList(@RequestParam("movie_id") Long movieId) {
+//        String username = customUserDetailsService.findLoggedInUsername();
+//        if (username != null) {
+//            Visitor user = allVisitors.findUserByUsername(username);
+//            List<Long> watchList = allVisitors.getWatchListByUser(user.getId());
+//            watchList.add(movieId);
+//            user.setWatchListMovieIds(watchList);
+//            allVisitors.save(user);
+//        }
+//    }
+
 }
