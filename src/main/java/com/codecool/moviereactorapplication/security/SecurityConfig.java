aad05439ad1bb4dead29efac1d30dcbd06efd760
Eszter Bokkon <bokkone@gmail.com>
2020-07-30 16:01:49 +0200

    Modify security config

--- src/main/java/com/codecool/moviereactorapplication/security/SecurityConfig.java
+++ src/main/java/com/codecool/moviereactorapplication/security/SecurityConfig.java
@@ -8,12 +8,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
+@CrossOrigin
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -27,25 +29,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .httpBasic().disable()
+                .cors().and()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/").permitAll()
-                .antMatchers("/auth/*").permitAll()
-                .antMatchers("/top").permitAll()
-                .antMatchers(HttpMethod.GET, "/scheduled-movies").permitAll()
-                .antMatchers("/reservation/*").hasRole("USER")
-                .antMatchers(HttpMethod.GET,"/room/*").permitAll()
-                .antMatchers(HttpMethod.GET,"/seat/room/*").permitAll()
-                .antMatchers(HttpMethod.GET,"/reserved-seats/show/*").permitAll()
-                .antMatchers(HttpMethod.GET,"/schedule").permitAll()
-                .antMatchers(HttpMethod.GET,"/show/*").permitAll()
-                .antMatchers(HttpMethod.PUT,"/show/*").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET,"/watchlist/user").hasRole("USER")
-                .antMatchers(HttpMethod.POST,"/watchlist/save/*").hasRole("USER")
-                .antMatchers(HttpMethod.DELETE,"/watchlist/delete/*").hasRole("USER")
-                .anyRequest().denyAll() // anything else is denied
+//                .antMatchers("/").permitAll()
+//                .antMatchers("/auth/*").permitAll()
+                .antMatchers("/*").permitAll()
+//                .antMatchers("/top").permitAll()
+//                .antMatchers(HttpMethod.GET, "/scheduled-movies").permitAll()
+//                .antMatchers("/reservation/*").hasRole("USER")
+//                .antMatchers(HttpMethod.GET,"/room/*").permitAll()
+//                .antMatchers(HttpMethod.GET,"/seat/room/*").permitAll()
+//                .antMatchers(HttpMethod.GET,"/reserved-seats/show/*").permitAll()
+//                .antMatchers(HttpMethod.GET,"/schedule").permitAll()
+//                .antMatchers(HttpMethod.GET,"/show/*").permitAll()
+//                .antMatchers(HttpMethod.PUT,"/show/*").hasRole("ADMIN")
+//                .antMatchers(HttpMethod.GET,"/watchlist/user").hasRole("USER")
+//                .antMatchers(HttpMethod.POST,"/watchlist/save").permitAll()
+//                .antMatchers(HttpMethod.DELETE,"/watchlist/delete/*").hasRole("USER")
+//                .anyRequest().denyAll() // anything else is denied
                 .and()
                 .addFilterBefore(new JwtTokenFilter(jwtTokenServices), UsernamePasswordAuthenticationFilter.class);
     }
@@ -61,8 +65,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedOrigin("http://localhost:3000");
         configuration.setAllowCredentials(true);
-        configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-*", "Origin", "Content-type", "Accept", "Authorization"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
+//        configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-*", "Origin", "Content-type", "Accept", "Authorization"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+//        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
